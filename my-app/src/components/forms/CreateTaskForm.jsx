a2466a79c2344383488df502553389d01926d886
veresrobert <veresrobert20@gmail.com>
2024-01-29 15:24:37 +0200

    introducing styles

--- my-app/src/components/forms/CreateTaskForm.jsx
+++ my-app/src/components/forms/CreateTaskForm.jsx
@@ -1,29 +1,31 @@
-import React from "react"
-import "./CreateTaskForm.css"
+import React from "react";
+import "./CreateTaskForm.css";
+
 const CreateTaskForm = () => {
-    return (
-      <div>
-        <form>
-          <div className="form-row">
-            <label>Task Name</label>
-            <input type="text"></input>
-          </div>
+  return (
+    <div>
+      <form>
+        <div className="form-row">
+          <label className="label-md">Task Name</label>
+          <input className="input-primary" type="text" />
+        </div>
 
-          <div className="form-row">
-            <label>Due Date</label>
-            <input type="text"></input>
-          </div>
+        <div className="form-row">
+          <label className="label-md">Due Date</label>
+          <input className="input-primary" type="date" />
+        </div>
 
-          <div className="form-row">
-            <label>Task Details</label>
-            <textarea name="" id="" cols={30} rows={3}></textarea>
-          </div>
+        <div className="form-row">
+          <label className="label-md">Task Details</label>
+          <textarea className="input-primary" cols="30" rows="10"></textarea>
+        </div>
 
-          <button type="submit">Create Task</button>
-        </form>
-      </div>
-    );
-}
-
+        <button className="button-primary" type="submit">
+          Create Task
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default CreateTaskForm;
